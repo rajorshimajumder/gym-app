10538205b42f9344b11d3f4fd3516d66dfb92a74
Rajorshi <mailto.rajorshimajumder@gmail.com>
2022-01-15 21:42:08 +0530

    progress chart and notifications

---
+++ gym_app_v1/lib/routes.dart
@@ -0,0 +1,13 @@
+import 'package:gym_app_v1/progressChart.dart';
+
+import 'notifications.dart';
+import 'profile.dart';
+import 'index.dart';
+
+class routes {
+  static const String profile = Profile.routeName;
+  static const String index = Index.routeName;
+  static const String notifications = Notifications.routeName;
+
+  static const String progressChart = ProgressChart.routeName;
+}
